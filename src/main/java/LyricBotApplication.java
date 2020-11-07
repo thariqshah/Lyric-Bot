c7d8c59ab9452264c40fb22143d89043500d1e6a
thariq <thariqshah@gmail.com>
2020-11-08 02:29:55 +0530

    Added !ping listener- Initial commit

---
+++ src/main/java/LyricBotApplication.java
@@ -0,0 +1,26 @@
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+import java.util.logging.Logger;
+
+
+public class LyricBotApplication {
+
+    public static Logger log = Logger.getLogger(LyricBotApplication.class.getName());
+
+    public static  void main(String[] args) {
+
+
+
+        DiscordApi api = new DiscordApiBuilder().setToken("Nzc0NzM2NzE5ODkwMzUwMDgw.X6cHyw._aZttAL58KQUhiWRraZegwTLgUA").login().join();
+        //Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().matches("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+        log.info("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
